882528b7e240b8b6464619a34406b3cc99fb1b3e
Jiya-dedhia-whj <94034939+Jiya-dedhia-whj@users.noreply.github.com>
2024-01-31 17:59:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,12 +24,25 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent("canvas");
+
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+  console.log("Model is Loaded");
 }
 
 
 function draw(){
 
  background(0); 
+ image(video,0,0,700,600);
 
  fill("black");
  stroke("black");
